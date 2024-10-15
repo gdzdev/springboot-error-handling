1730bdc15f5d85177651de955ea980ac13112107
aleksGdz <aleksgaldamez2002@gmail.com>
2024-10-14 23:18:23 -0600

    initial commit

---
+++ src/test/java/org/agaldamez/springboot/error/handling/SpringBootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agaldamez.springboot.error.handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
